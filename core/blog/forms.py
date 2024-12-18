b77dc48de9c14471537880640966964804d28971
CYR3S <amingholami06@gmail.com>
2024-12-18 11:51:39 -0800

    Add some Views and Permissions
    In this session, I add some views like FormView, CreateView... and some auth.mixins like LoginNRequiredMixin and PermissionRequiredMixin.

---
+++ core/blog/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Post
+
+# Create form to make posts
+class PostForm(forms.ModelForm):
+    """
+    This class has used for for creating posts. It gets
+    model(like Post in the bottom) to recognize the process
+    it should do. Then, you can choose which fields you want
+    to add to create a post. 
+    """
+    class Meta:
+        model = Post
+        fields = ["author", "title", "content",
+                  "status", "category", "published_date"]
+
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView , DetailView
+from django.views.generic import ListView , DetailView, FormView, CreateView , UpdateView , DeleteView
 from .models import Post
+from .forms import PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 # Create index view for the main blog page
 def indexView(request):
@@ -26,12 +28,13 @@ class IndexView(TemplateView):
         return context
 
 # ListView of Posts
-class PostList(ListView):
+class PostList(PermissionRequiredMixin, LoginRequiredMixin, ListView):
     """
     This class is for Posts that uses ListView.
     """
     # model = Post
     # queryset = Post.objects.all()
+    permission_required = "blog.view_post"
     paginate_by = 2
     context_object_name = 'posts'
     # ordering = "-id"
@@ -40,8 +43,53 @@ class PostList(ListView):
         return posts
 
 # DetailView of posts
-class PostDetailView(DetailView):
+class PostDetailView(PermissionRequiredMixin, LoginRequiredMixin, DetailView):
     """
     This class is for detail of a post in blog.
     """
     model = Post
+    permission_required = "blog.view_post"
+    
+    
+"""
+# FormView
+class PostCreateView(FormView):
+    template_name = "contact.html"
+    form_class = PostForm
+    success_url = "/blog/post/"
+    
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+"""
+
+# CreateView for making new posts
+class PostCreateView(PermissionRequiredMixin, LoginRequiredMixin, CreateView):
+    permission_required = "blog.create_post"
+    model = Post
+    fields = ["title", "content",
+                "status", "category", "published_date"]
+    success_url = "/blog/post/"
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+# UpdateView for editing posts
+class PostEditView(UpdateView):
+    """
+    This class is used for editing posts.
+    """
+    model = Post
+    form_class = PostForm
+    success_url = "/blog/post/"
+
+# DeleteView for delete posts in blog
+class PostDeleteView(PermissionRequiredMixin, LoginRequiredMixin, DeleteView):
+    """
+    This class is used for deleting posts. It gets model
+    to recognize it. Then, gets a url to get back to the page
+    you wanna go.
+    """
+    model = Post
+    success_url = "/blog/post/"
